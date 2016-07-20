b5e138ce6cad442629ef53a6be0d281b93ae39b9
addamm12 <adamnet@vp.pl>
2016-07-20 14:57:37 +0200

    Appium

--- src/test/java/com/sql/Test1.java
+++ src/test/java/com/sql/Test1.java
@@ -1,6 +1,7 @@
 package com.sql;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,10 +9,14 @@ import java.sql.Statement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Test1 {
+public class test1 {
+	
    @Test
    public void Test1() throws SQLException{
-	    Connection vConnection=SQL.getConnection();   
+		String host="localhost";
+		String port="3306";
+		Connection vConnection = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/world?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
+	    //Connection vConnection=SQL.getConnection();   
 		Statement vStatemanet=vConnection.createStatement();
 		ResultSet vResultSet=vStatemanet.executeQuery("select * from city where Population>9000000");
 		vResultSet.next();
